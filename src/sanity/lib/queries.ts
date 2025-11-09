2c440a938abb89b5586e2132cc3a006db1dd400b
anujvishwakarma07 <anujvishwakarma7077@gmail.com>
2025-11-09 14:00:30 +0530

    Some authentication issues fixes

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -47,15 +47,27 @@ export const STARTUP_VIEWS_QUERY = defineQuery(`*[_type == "startup" && _id == $
 }`);
 
 
-export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`*[_type == "author" && id == $id][0]{
+// export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`*[_type == "author" && id == $id][0]{
+//     _id,
+//     id,
+//     name,
+//     username,
+//     email,
+//     image,
+//     bio
+// }`)
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
+  *[_type == "author" && githubId == $id][0]{
     _id,
-    id,
+    githubId,
     name,
     username,
     email,
     image,
     bio
-}`)
+  }
+`)
+
 export const AUTHOR_BY_ID_QUERY = defineQuery(`*[_type == "author" && _id == $id][0]{
     _id,
     id,
